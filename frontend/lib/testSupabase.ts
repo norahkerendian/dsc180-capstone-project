9eab80dca78bd87094de2f8e3bc3b86ef2e79f0a
Sadrac Santacruz <70788009+SadracSantacruz@users.noreply.github.com>
2026-01-31 20:05:28 -0800

    testing

--- frontend/lib/testSupabase.ts
+++ frontend/lib/testSupabase.ts
@@ -2,8 +2,14 @@ import supabase from "./supabaseClient";
 
 export async function getQuestions() {
   const { data, error } = await supabase.from("questions").select("*");
+
+  if (error) {
+    console.error("ERROR:", error);
+    return null;
+  }
+
   console.log("DATA:", data);
-  console.log("ERROR:", error);
+  return data;
 }
 
 getQuestions();
